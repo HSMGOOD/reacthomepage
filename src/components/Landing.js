a3ebdec76e8221ec7ef4e79b7f220323c05cd627
Sungmin <gktjdals1243@gmail.com>
2022-02-25 18:16:49 +0900

    dsds

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./head/Header";
+import Footer from './foot/Footer'
+
+function Landing() {
+  return (
+    <>
+        <Header></Header>
+        <div></div>
+        <Footer></Footer>
+    </>
+  )
+}
+
+export default Landing
---
+++ src/components/head/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderTitle>Header</HeaderTitle>
+  )
+}
+
+export default Header
+
+const HeaderTitle = styled.div`
+    background-color: rgb(33, 35, 42);
+`
